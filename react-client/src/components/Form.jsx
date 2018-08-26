11399b4ba6af6b9067eacf752ff3989ce5cb66cf
Tong Allan Zhao <allanzhao@Tongs-MacBook-Air.local>
2018-08-25 17:54:52 -0700

    Implement handleSubmit and handleChange methods
    handleSubmit will alert the user that their message will be sent to the phone number specified at the specified time when the user clicks on the submit button.
    
    handleChange will keep track of user input to the phone number, message and sendAt fields and update the state accordingly.

--- react-client/src/components/Form.jsx
+++ react-client/src/components/Form.jsx
@@ -7,9 +7,33 @@ class Form extends React.Component {
     this.state = {
       phoneNumber: '',
       message: '',
-      date: '',
+      sendAt: '',
     }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.value) {
+      this.setState({
+        phoneNumber: event.value
+      });
+    } else {
+      const target = event.target;
+      const value = target.value;
+      const name = target.name;
+
+      this.setState({
+        [name]: value
+      });
+      
+    }
+  }
+
+  handleSubmit(event) {
+    alert(`Your message will be sent to ${this.state.phoneNumber} at ${this.state.sendAt}`);
+    event.preventDefault();
   }
 
   render () {
@@ -17,14 +41,31 @@ class Form extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Phone number: {' '}
+          <NumberFormat
+            format="(###) ###-####"
+            mask=""
+            name="phoneNumber"
+            placeholder="Enter 10-digit phone number"
+            onValueChange={this.handleChange}
+            value={this.state.phoneNumber} />
         </label>
         <br />
         <label>
           Message: {' '}
+          <textarea
+            name="message"
+            type="text"
+            placeholder="Type your message here!"
+            value={this.state.message}
+            onChange={this.handleChange} />
         </label>
         <br />
         <label>
           Choose when to send this message: {' '}
+          <input
+            name="sendAt"
+            type="time"
+            onChange={this.handleChange} />
         </label>
         <br />
         <input type="submit" value="Submit" />
