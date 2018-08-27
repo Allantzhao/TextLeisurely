546d2337a990b425f141eef54840ae0d9adb4ad3
Tong Allan Zhao <allanzhao@Tongs-MacBook-Air.local>
2018-08-27 13:43:27 -0700

    Setup twilio with node js to send a message to a specified phone number

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+var client = require('twilio')(accountSid, authToken);
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -6,6 +10,14 @@ var items = require('../database');
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+
+client.messages.create({
+  to: process.env.CELL_PHONE_NUMBER,
+  from: process.env.TWILIO_PHONE_NUMBER,
+  body: 'hello',
+}).then((message) => {
+  console.log(message.sid);
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
